cc5828068c6e63d7b371b62191944a6dd48c8214
Maya Tatarelli <49986410+MayaTatarelli@users.noreply.github.com>
2022-06-14 17:32:54 -0400

    Moved plotting to DG_plotting.py file out of run_DG

--- DG_plotting.py
+++ DG_plotting.py
@@ -57,3 +57,74 @@ for i in range(0,n_time_steps+1):
 	    title_label=current_time_string,
 	    legend_labels_tex=["solution","solution"])
 '''
+'''
+#CONVERGENCE
+x_values = (2.0*np.pi)/num_elements_values
+print(x_values)
+error_values = np.loadtxt("error_values_lower_num_elmnts_2.csv", delimiter=",")
+print(error_values)
+poly_degree = 2
+
+def get_ref_curve(error_vals,x_data,n):
+    # n is order
+    index_align = -1
+    y_align = error_vals[index_align]
+    x_align = x_data[index_align]
+	# n = -(P+1)
+    shift = np.log10(1.0/(y_align*((x_align*10.0)**float(n))))
+	# Confirm the order of accuracy
+	# shift = [-1.0,-2.0]
+	# for n in range(1,3): 
+	# 	name = r"$h^{-%i}$" % n
+	# 	dx_n = (dx_store**n)*10.0**(shift[n-1]+float(n))
+	# 	plt.loglog(1.0/dx_store,dx_n,label=name,color=clr[n+2],linewidth=1.0,linestyle='--')
+    dx_n = (x_data**n)*10.0**(shift+float(n))
+    return dx_n
+
+plt.figure()
+i = poly_degree+1
+dx_n = get_ref_curve(error_values, x_values, -i)
+label_name = "$\\Delta x^{-%i}$" % i
+plt.loglog(1.0/x_values, 1.0/dx_n,linestyle="--",label=label_name)
+
+plt.loglog(1.0/x_values, error_values)#,label=name_scheme, linestyle=linestyles[i_scheme])
+
+plt.legend()
+plt.show()
+'''
+
+'''
+num_to_remove = 10
+plt.figure()
+i = poly_degree+1
+dx_n = get_ref_curve(error_values[:-num_to_remove], x_values[:-num_to_remove], -i)
+label_name = "$\\Delta x^{-%i}$" % i
+plt.loglog(1.0/x_values[:-num_to_remove], 1.0/dx_n,linestyle="--",label=label_name)
+
+plt.loglog(1.0/x_values[:-num_to_remove], error_values[:-num_to_remove])#,label=name_scheme, linestyle=linestyles[i_scheme])
+
+plt.legend()
+plt.show()
+'''
+'''
+high_num_elements_values = [5]
+for i in range(1,10):
+	high_num_elements_values.append(high_num_elements_values[i-1]+10)
+high_num_elements_values = np.array(high_num_elements_values)
+print(high_num_elements_values)
+
+high_x_values = (2.0*np.pi)/high_num_elements_values
+high_error_values = np.loadtxt("error_values.csv", delimiter=",")
+poly_degree = 2
+
+plt.figure()
+i = poly_degree+1
+high_dx_n = get_ref_curve(high_error_values, high_x_values, -i)
+label_name = "$\\Delta x^{-%i}$" % i
+plt.loglog(1.0/high_x_values, 1.0/high_dx_n,linestyle="--",label=label_name)
+
+plt.loglog(1.0/high_x_values, high_error_values)#,label=name_scheme, linestyle=linestyles[i_scheme])
+
+plt.legend()
+plt.show()
+'''
