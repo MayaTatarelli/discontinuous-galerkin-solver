e12eb6c48c3db4d67472aefc199d983f07abb129
Cesare Tatarelli <cesare@Cesares-iMac.local>
2022-06-21 17:48:25 -0400

    added updated titles for output files when running DG solver

--- run_DG_convergence.py
+++ run_DG_convergence.py
@@ -1,8 +1,8 @@
 import numpy as np
 import DGSolver
 import sys
-sys.path.append('../quickplotlib/lib/')
-from quickplotlib import plotfxn, plot_matrix_sparsity_pattern
+#sys.path.append('../quickplotlib/lib/')
+#from quickplotlib import plotfxn, plot_matrix_sparsity_pattern
 import matplotlib . pyplot as plt
 
 number_of_runs = 10
@@ -17,18 +17,31 @@ num_elements_values = np.array(num_elements_values)
 
 error_values = -1.0*np.ones(number_of_runs)
 
+#Variables to be set for DGSolver
+cur_poly_degree = 2
+cur_cfl = 0.0015
+cur_cfl_name = "0015"
+cur_rk4 = True
+
+#Set filename
+if(cur_rk4):
+	file_name = "error_values_P{}_cfl{}_rk4.csv".format(cur_poly_degree,cur_cfl_name)
+else:
+	file_name = "error_values_P{}_cfl{}.csv".format(cur_poly_degree,cur_cfl_name)
+print(file_name)
+
 for iElements in range(len(num_elements_values)):
 	print("Running dg_solver with " + str(num_elements_values[iElements]) + " elements;")
 	dg_solver = DGSolver.DGSolver(domain_left=0.0,
 								  domain_right=(2.0*np.pi),
-								  poly_degree=1,
+								  poly_degree=cur_poly_degree,
 								  number_of_elements=num_elements_values[iElements],
-								  cfl=0.0075,
+								  cfl=cur_cfl,
 								  a=(2.0*np.pi),
-								  rk4=True)
+								  rk4=cur_rk4)
 	error_values[iElements] = dg_solver.advance_solution_time()
 	print("Error: " + str(error_values[iElements]))
-	np.savetxt("error_values_P1_cfl0075_rk4.csv", error_values, delimiter=',') #, fmt='%d')
+	np.savetxt(file_name, error_values, delimiter=',') #, fmt='%d')
 
 # print(error_values)
 # np.savetxt("error_values_P2_cfl0015.csv", error_values, delimiter=',') #, fmt='%d')
